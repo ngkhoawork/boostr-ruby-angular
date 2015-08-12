1473097ab5a4864fd73b1b54b24bde0d679de0b8
jcomplex <jcomplex@gmail.com>
2015-08-11 23:25:32 -0400

    As a User I can reset my password. [Finishes #101302864]

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -6,6 +6,24 @@ module DeviseHelper
     notice: 'alert-info'
   }
 
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t('errors.messages.not_saved',
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_explanation">
+      <h2>#{sentence}</h2>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
   def bootstrap_class_for(flash_type)
     BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type.to_s)
   end
