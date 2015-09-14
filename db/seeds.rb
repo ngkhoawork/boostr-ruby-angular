ab954fbf915e931df7b2b752225451fbf68c08c3
jeffrafter <jeffrafter@gmail.com>
2015-09-14 13:23:54 -0700

    Begin adding some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,72 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin_user = User.create!(
+  first_name: "Admin",
+  last_name: "User",
+  email: "admin@example.com",
+  password: "password"
+)
+
+company_user = User.create!(
+  first_name: "Company",
+  last_name: "User",
+  email: "company@example.com",
+  password: "password"
+)
+
+team_leader_users = User.create!([
+  {
+    first_name: "Leader",
+    last_name: "User",
+    email: "leader@example.com",
+    password: "password"
+  }
+])
+
+team_member_users = User.create!([
+  {
+    first_name: "Member",
+    last_name: "User",
+    email: "member@example.com",
+    password: "password"
+  }
+])
+
+company = Company.create!(
+  name: "Acme Corporation",
+  billing_address_attributes: {
+    street1: '15 Beverly',
+    city: 'Los Angeles',
+    state: 'CA',
+    zip: '90210',
+    email: 'hello@example.com',
+    website: 'http://example.com',
+    phone: '310-555-1212',
+    mobile: '310-555-1212'
+  },
+  physical_address_attributes: {
+    street1: '15 Beverly',
+    city: 'Los Angeles',
+    state: 'CA',
+    zip: '90210',
+    email: 'hello@example.com',
+    website: 'http://example.com',
+    phone: '310-555-1212',
+    mobile: '310-555-1212'
+  },
+  primary_contact_id: company_user.id,
+  billing_contact_id: company_user.id
+)
+
+Product.create!([
+  { company: company, name: "Video", product_line: "Phone", family: "Native", pricing_type: "CPM" },
+  { company: company, name: "Native Ad - Mobile", product_line: "Tablet", family: "Banner",  pricing_type: "CPC" },
+  { company: company, name: "Native Ad - Desktop", product_line: "Desktop", family: "Native", pricing_type: "CPC" }
+])
+
+# Stages
+
+# Teams
+# Clients
+# Contacts
+# Deals
+# Time periods
+# Quotas
