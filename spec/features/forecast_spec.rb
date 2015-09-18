0d98a4209fccf68aec9b55d6780a7164acb838a3
Alfie La Peter <alfie@alfielapeter.com>
2015-09-18 14:02:00 -0600

    As a user I can change the time period for the forecast
    [Finishes #102423314]

--- spec/features/forecast_spec.rb
+++ spec/features/forecast_spec.rb
@@ -7,6 +7,7 @@ feature 'Forecast' do
   let!(:child) { create :child_team, company: company, parent: parent }
   let!(:member) { create :user, company: company, team: child }
   let!(:time_period) { create :time_period, company: company }
+  let!(:another_time_period) { create :time_period, company: company, name: 'Q2' }
 
 
   describe 'showing the root level of teams' do
@@ -17,17 +18,39 @@ feature 'Forecast' do
       expect(page).to have_css('#forecasts')
     end
 
-    scenario 'shows the parent team name' do
-       within '.table-wrapper tbody' do
+    scenario 'shows the parent team name and drills down teams and changes time periods' do
+      within '.table-wrapper tbody' do
         expect(page).to have_css('tr', count: 1)
 
         within 'tr:first-child' do
           expect(find('td:first-child')).to have_text parent.name
           find('td:first-child a').click
           expect(find('td:first-child')).to have_text child.name
+        end
+      end
+
+      within '.quota-period' do
+        find('a').click
+
+        within('.dropdown-menu') do
+          find('li:last-child a').click
+        end
+
+        expect(page).to have_text 'Q2'
+      end
+
+      within '.table-wrapper tbody' do
+        expect(page).to have_css('tr', count: 1)
+
+        within 'tr:first-child' do
+          expect(find('td:first-child')).to have_text child.name
           find('td:first-child a').click
           expect(find('td:first-child')).to have_text member.full_name
         end
+      end
+
+      within '.quota-period' do
+        expect(page).to have_text 'Q2'
       end
     end
   end
