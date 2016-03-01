36aeb666b19e57557291e5f68fd6467c1ed577ec
James Wong <jameshsw@yahoo.com>
2016-03-01 09:28:09 -0800

    Notification email format

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'Boostr <noreply@boostrcrm.com>'
+  default from: 'boostr <noreply@boostrcrm.com>'
  
   def close_email(recipients, subject, deal)
     @deal = deal
