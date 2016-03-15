3e28dfab246c9dc078b427c532c26e943c3ea496
James Wong <jameshsw@yahoo.com>
2016-03-15 10:16:12 -0700

    Store run rate precision

--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -123,10 +123,10 @@ class Revenue < ActiveRecord::Base
   def set_alert
     if !budget.nil? && !budget_remaining.nil?
       if budget > 0 && start_date < DateTime.now && DateTime.now < end_date
-        self.run_rate = (budget-budget_remaining)/(DateTime.now.to_date-start_date.to_date+1)
+        self.run_rate = (budget-budget_remaining)/(DateTime.now.to_date-start_date.to_date+1)*100
         if self.run_rate != 0
-          self.remaining_day = budget_remaining/self.run_rate
-          self.balance = ((end_date.to_date-DateTime.now.to_date+1)-self.remaining_day)*self.run_rate
+          self.remaining_day = budget_remaining/self.run_rate/100
+          self.balance = ((end_date.to_date-DateTime.now.to_date+1)-self.remaining_day)*self.run_rate/100
         else
           self.remaining_day = 0
           self.balance = 0
