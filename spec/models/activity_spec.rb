ce0a2e4767127f5b370bc7ecd8b873bec33651ef
James Wong <jameshsw@yahoo.com>
2016-04-22 15:30:30 -0700

    Client json fix and activities spec

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
   describe '#add_activity' do
-    let(:activity) { create :activity, happened_at: Date.new(2016, 3, 31) }
+    let(:company) { create :company }
+    let(:activity) { create :activity, happened_at: Date.new(2016, 3, 31), company: company }
+    let!(:client) { create :client, company: company }
+    let!(:deal) { create_list :deal, 2, company: company, advertiser: client }
 
     it 'return activitiy date' do
       expect(activity.happened_at).to eq(Date.new(2016, 3, 31))
