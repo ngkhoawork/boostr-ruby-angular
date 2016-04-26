aa669e1961f8aa0e209eb23f0b052550f4514bf5
James Wong <jameshsw@yahoo.com>
2016-04-25 19:05:57 -0700

    Activities spec

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Activity, type: :model do
   describe '#add_activity' do
     let(:company) { create :company }
-    let(:activity) { create :activity, happened_at: Date.new(2016, 3, 31), company: company }
-    let!(:client) { create :client, company: company }
-    let!(:deal) { create_list :deal, 2, company: company, advertiser: client }
+    let(:client) { create :client, company: company }
+    let(:deal) { create :deal, company: company, advertiser: client }
+    let(:activity) { create :activity, deal: deal, happened_at: Date.new(2016, 3, 31), company: company }
 
     it 'return activitiy date' do
       expect(activity.happened_at).to eq(Date.new(2016, 3, 31))
