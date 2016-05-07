50a43826025bc1223d4b7787b59193a00c8b23ce
James Wong <jameshsw@yahoo.com>
2016-05-06 18:12:44 -0700

    Transform report

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,23 +5,37 @@ class Report < ActiveRecord::Base
 
   def self.to_csv(company)
     CSV.generate do |csv|
-      csv << ["Time Period", "Seller Name/Total", "Name", "Value"]
+      header = []
+      header << "Time Period"
+      header << "Name"
+      company.activity_types.each do |a|
+        header << a.name
+      end
+      header << "Total"
+      csv << header
       company.time_periods.order(:name).each do |t|
-        company.activity_types.each do |a|
-          r = all.where("time_period_id = ? and user_id = ? and name = ?", t.id, -1, a.name).first
-          csv << [t.name, 'Total', a.name, r.nil? ? 0:r.value]
-        end
-        r = all.where("time_period_id = ? and user_id = ? and name = ?", t.id, -1, 'Total').first
-        csv << [t.name, 'Total', 'Total', r.nil? ? 0:r.value]
         company.users.order(:first_name).each do |u|
+          line = [t.name]
+          line << u.name
           company.activity_types.each do |a|
             r = all.where("time_period_id = ? and user_id = ? and name = ?", t.id, u.id, a.name).first
-            csv << [t.name, u.name, a.name, r.nil? ? 0:r.value]
+            line << (r.nil? ? 0:r.value)
           end
           r = all.where("time_period_id = ? and user_id = ? and name = ?", t.id, u.id, 'Total').first
-          csv << [t.name, u.name, 'Total', r.nil? ? 0:r.value]
+          line << (r.nil? ? 0:r.value)
+          csv << line
         end
+        line = [t.name]
+        line << 'Total'
+        company.activity_types.each do |a|
+          r = all.where("time_period_id = ? and user_id = ? and name = ?", t.id, -1, a.name).first
+          line << (r.nil? ? 0:r.value)
+        end
+        r = all.where("time_period_id = ? and user_id = ? and name = ?", t.id, -1, 'Total').first
+        line << (r.nil? ? 0:r.value)
+        csv << line
       end
     end
   end
+
 end
