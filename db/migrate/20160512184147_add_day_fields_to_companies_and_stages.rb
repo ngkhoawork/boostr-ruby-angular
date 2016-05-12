93fd3deab73d078096d8d5d1d8f0b69c4d4ccf48
Mike McTiernan <mike@mikemct.com>
2016-05-12 12:07:40 -0700

    Update day field migration to use integers

--- db/migrate/20160512184147_add_day_fields_to_companies_and_stages.rb
+++ db/migrate/20160512184147_add_day_fields_to_companies_and_stages.rb
@@ -1,12 +1,12 @@
 class AddDayFieldsToCompaniesAndStages < ActiveRecord::Migration
   def change
-    add_column :companies, :avg_day, :string
-    add_column :companies, :day1, :string
-    add_column :companies, :day2, :string
-    add_column :companies, :day3, :string
-    add_column :stages, :avg_day, :string
-    add_column :stages, :day1, :string
-    add_column :stages, :day2, :string
-    add_column :stages, :day3, :string
+    add_column :companies, :avg_day, :integer
+    add_column :companies, :day1, :integer
+    add_column :companies, :day2, :integer
+    add_column :companies, :day3, :integer
+    add_column :stages, :avg_day, :integer
+    add_column :stages, :day1, :integer
+    add_column :stages, :day2, :integer
+    add_column :stages, :day3, :integer
   end
 end
