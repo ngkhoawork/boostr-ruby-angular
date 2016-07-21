666146e0cb8575d74066198d8cfede388864f2de
Nikolay Andreev <n.andreev.211@gmail.com>
2016-07-21 13:25:22 +0100

    fix average deal size

--- app/controllers/api/kpis_controller.rb
+++ app/controllers/api/kpis_controller.rb
@@ -19,11 +19,11 @@ class Api::KpisController < ApplicationController
 
       average_deal_size = complete_deals.average(:budget) if complete_deals_count > 0
 
-      cycle_time_arr = complete_deals.collect{|deal| deal.closed_at - deal.created_at}
+      cycle_time_arr = complete_deals.collect{|deal| DateTime.parse(deal.closed_at.to_s)  - DateTime.parse(deal.created_at.to_s)}
       cycle_time = cycle_time_arr.sum.to_f / cycle_time_arr.count if cycle_time_arr.count > 0
 
       user.win_rate = win_rate.round(2) if win_rate > 0
-      user.average_deal_size = average_deal_size.round(2) if average_deal_size.present? && average_deal_size > 0
+      user.average_deal_size = (average_deal_size / 100.0).round(2) if average_deal_size.present? && average_deal_size > 0
       user.cycle_time = cycle_time.round(2) if cycle_time > 0
       user.save!
     end
