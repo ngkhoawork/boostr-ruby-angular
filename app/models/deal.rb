6951fd8b7fcc44ba238ef72bb2ef5fb0b07a7aa9
Nikolay Andreev <n.andreev.211@gmail.com>
2016-07-21 21:49:46 +0100

    fix 90 days boundary fix

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -61,7 +61,7 @@ class Deal < ActiveRecord::Base
 
   scope :for_client, -> (client_id) { where('advertiser_id = ? OR agency_id = ?', client_id, client_id) if client_id.present? }
   scope :for_time_period, -> (start_date, end_date) { where('deals.start_date <= ? AND deals.end_date >= ?', end_date, start_date) }
-  scope :closed_in, -> (duration_in_days) { where('deals.closed_at >= ?', Time.now - duration_in_days.days) }
+  scope :closed_in, -> (duration_in_days) { where('deals.closed_at >= ?', Time.now.beginning_of_day - duration_in_days.days) }
   scope :open, -> { joins(:stage).where('stages.open IS true') }
   scope :closed, -> { joins(:stage).where('stages.open IS false') }
   scope :at_percent, -> (percentage) { joins(:stage).where('stages.probability = ?', percentage) }
