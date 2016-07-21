7f07a4b95103d00087c25be505b7672b614356db
Nikolay Andreev <n.andreev.211@gmail.com>
2016-07-22 00:39:18 +0100

    add 1 to cycle time calculation

--- app/controllers/api/kpis_controller.rb
+++ app/controllers/api/kpis_controller.rb
@@ -18,7 +18,7 @@ class Api::KpisController < ApplicationController
       average_deal_size = complete_deals.average(:budget) if complete_deals_count > 0
 
       cycle_time_arr = complete_deals.collect{|deal| Date.parse(DateTime.parse(deal.closed_at.to_s).utc.to_s)  - Date.parse(deal.created_at.utc.to_s)}
-      cycle_time = cycle_time_arr.sum.to_f / cycle_time_arr.count if cycle_time_arr.count > 0
+      cycle_time = (cycle_time_arr.sum.to_f / cycle_time_arr.count + 1) if cycle_time_arr.count > 0
 
       user.win_rate = win_rate.round(2) if win_rate > 0
       user.average_deal_size = (average_deal_size / 100.0).round(2) if average_deal_size.present? && average_deal_size > 0
