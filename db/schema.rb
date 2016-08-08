ad8ea80af9829691d7cbd6be970613eb6f43a250
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2016-08-08 13:42:53 +0300

    Added acts_as_paranoid support for Reminders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803121143) do
+ActiveRecord::Schema.define(version: 20160808104151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -288,7 +288,10 @@ ActiveRecord::Schema.define(version: 20160803121143) do
     t.datetime "remind_on"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.datetime "deleted_at"
   end
+
+  add_index "reminders", ["deleted_at"], name: "index_reminders_on_deleted_at", using: :btree
 
   create_table "reports", force: :cascade do |t|
     t.integer  "company_id"
