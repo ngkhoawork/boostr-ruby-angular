3fc7c9b07c7755344dc2c5a3acc4d7f537ea1740
Nikolay Andreev <n.andreev.211@gmail.com>
2016-08-27 01:17:28 +0300

    remove google event id uniqueness validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -15,7 +15,7 @@ class Activity < ActiveRecord::Base
   has_many :assets, as: :attachable
 
   validates :company_id, presence: true
-  validates_uniqueness_of :google_event_id
+  # validates_uniqueness_of :google_event_id
 
   after_create do
     if !deal_id.nil?
