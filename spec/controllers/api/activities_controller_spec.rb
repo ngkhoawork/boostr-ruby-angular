c08f8e32993817e8841d607a85e7e6eca53957a3
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2016-09-04 22:06:31 +0300

    Updated activity tests and optimized contact creation

--- spec/controllers/api/activities_controller_spec.rb
+++ spec/controllers/api/activities_controller_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe Api::ActivitiesController, type: :controller do
   let(:user) { create :user, company: company, team: team }
   let(:client) { create :client, company: company }
   let(:deal) { create :deal, advertiser: client, company: company }
-  let(:contact) { create :contact, company: company, client: client }
+  let(:contacts) { create_list :contact, 10, company: company, client: client }
   let(:activity_params) {
     attributes_for(:activity)
   }
+  let(:existing_activity) { create :activity, company: company }
 
   before do
     sign_in user
@@ -20,12 +21,25 @@ RSpec.describe Api::ActivitiesController, type: :controller do
       expect {
         post :create, {
           activity: activity_params,
-          contacts: [contact.id]
+          contacts: contacts.map(&:id)
         }, format: :json
         expect(response).to be_success
         response_json = JSON.parse(response.body)
-        expect(response_json['contacts'].first['id']).to eq(contact.id)
+        expect(response_json['contacts'].length).to be 10
       }.to change(Activity, :count).by(1)
+    end
+  end
+
+  describe "PUT #update" do
+    it 'creates a new activity and returns success' do
+      put :update, {
+        id: existing_activity.id,
+        activity: activity_params,
+        contacts: contacts.map(&:id)
+      }, format: :json
+      expect(response).to be_success
+      response_json = JSON.parse(response.body)
+      expect(response_json['contacts'].length).to be 10
     end
   end
 end
