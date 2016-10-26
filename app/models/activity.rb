1ed33dbec4c9c99981e8dbfe911dc12f1ddb38fa
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2016-10-26 20:26:16 +0300

    Added activity import code

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -27,6 +27,156 @@ class Activity < ActiveRecord::Base
     end
   end
 
+  def self.import(file, current_user)
+    errors = []
+
+    row_number = 0
+    CSV.parse(file, headers: true) do |row|
+      row_number += 1
+
+      if row[1].present?
+        begin
+          happened_at = DateTime.parse(row[1])
+        rescue ArgumentError
+          error = {row: row_number, message: ['Date must be a valid datetime'] }
+          errors << error
+          next
+        end
+      else
+        error = { row: row_number, message: ['Date is empty'] }
+        errors << error
+        next
+      end
+
+      if row[2].nil? || row[2].blank?
+        error = { row: row_number, message: ['Creator is empty'] }
+        errors << error
+        next
+      else
+        creator = current_user.company.users.where('email ilike ?', row[2]).first
+        unless creator
+          error = { row: row_number, message: ["User #{row[2]} could not be found"] }
+          errors << error
+          next
+        end
+      end
+
+      if row[3].present?
+        advertiser_type_id = Client.advertiser_type_id(current_user.company)
+        advertisers = current_user.company.clients.by_type_id(advertiser_type_id).where('name ilike ?', row[3].strip)
+        if advertisers.length > 1
+          error = { row: row_number, message: ["Advertiser #{row[3]} matched more than one client record"] }
+          errors << error
+          next
+        elsif advertisers.length == 0
+          error = { row: row_number, message: ["Advertiser #{row[3]} could not be found"] }
+          errors << error
+          next
+        else
+          advertiser = advertisers.first
+        end
+      end
+
+      if row[4].present?
+        agency_type_id = Client.agency_type_id(current_user.company)
+        agencies = current_user.company.clients.by_type_id(agency_type_id).where('name ilike ?', row[4].strip)
+        if agencies.length > 1
+          error = { row: row_number, message: ["Agency #{row[4]} matched more than one client record"] }
+          errors << error
+          next
+        elsif agencies.length == 0
+          error = { row: row_number, message: ["Agency #{row[4]} could not be found"] }
+          errors << error
+          next
+        else
+          agency = agencies.first
+        end
+      end
+
+      if row[5].present?
+        deals = current_user.company.deals.where('name ilike ?', row[5].strip)
+        if deals.length > 1
+          error = { row: row_number, message: ["Deal #{row[5]} matched more than one deal record"] }
+          errors << error
+          next
+        elsif deals.length == 0
+          error = { row: row_number, message: ["Deal #{row[5]} could not be found"] }
+          errors << error
+          next
+        else
+          deal = deals.first
+        end
+      end
+
+      if row[6].present?
+        type = current_user.company.activity_types.where('name ilike ?', row[6].strip).first
+        unless type
+          error = { row: row_number, message: ["Activity type #{row[6]} could not be found"] }
+          errors << error
+          next
+        end
+      else
+        error = { row: row_number, message: ['Activity type is empty'] }
+        errors << error
+        next
+      end
+
+      contact_list = []
+      if row[8].present?
+        contact_emails = row[8].split(';')
+
+        contact_list_error = false
+        contact_emails.each do |email|
+          contact = Contact.by_email(email, current_user.company_id).first
+          if contact
+            contact_list << contact
+          else
+            error = { row: row_number, message: ["Activity contact #{email} could not be found in the contacts list"] }
+            errors << error
+            contact_list_error = true
+            break
+          end
+        end
+
+        if contact_list_error
+          next
+        end
+      end
+
+      activity_params = {
+        user: creator,
+        creator: creator,
+        updator: creator,
+        deal: deal,
+        client: advertiser,
+        agency: agency,
+        activity_type_name: type.name,
+        happened_at: happened_at,
+        comment: row[7] ? row[7].strip : nil
+      }
+
+      if row[0]
+        begin
+          activity = current_user.company.activities.find(row[0])
+        rescue ActiveRecord::RecordNotFound
+        end
+      end
+      if !(activity.present?)
+        activity = current_user.company.activities.new
+      end
+
+      if activity.update_attributes(activity_params)
+        activity.contacts << contact_list
+      else
+        error = { row: row_number, message: activity.errors.full_messages }
+        errors << error
+        next
+      end
+    end
+
+    errors
+  end
+
   def as_json(options = {})
     super(options.merge(
       include: {
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -10,5 +10,172 @@ RSpec.describe Activity, type: :model do
     it 'return activity date' do
       expect(activity.happened_at).to eq(Date.new(2016, 3, 31))
     end
+
+    describe '#import' do
+      let!(:user) { create :user }
+      let!(:company) { user.company }
+      let!(:advertiser) { create :client, created_by: user.id, client_type_id: advertiser_type_id(company) }
+      let!(:agency) { create :client, created_by: user.id, client_type_id: agency_type_id(company) }
+      let!(:deal) { create :deal, name: 'New Big Deal' }
+      let!(:contacts) { create_list :contact, 4, company: company }
+      let!(:activity) { create :activity }
+
+      it 'creates a new activity from csv' do
+        data = build :activity_csv_data, agency: agency.name, advertiser: advertiser.name
+        expect do
+          expect(Activity.import(generate_csv(data), user)).to eq([])
+        end.to change(Activity, :count).by(1)
+
+        new_activity = Activity.last
+        expect(new_activity.happened_at).to eq(data[:date])
+        expect(new_activity.creator.email).to eq(data[:creator])
+        expect(new_activity.updator.email).to eq(data[:creator])
+        expect(new_activity.user.email).to eq(data[:creator])
+        expect(new_activity.client.name).to eq(data[:advertiser])
+        expect(new_activity.agency.name).to eq(data[:agency])
+        expect(new_activity.deal.name).to eq(data[:deal])
+        expect(new_activity.activity_type_name).to eq(data[:type])
+        expect(new_activity.comment).to eq(data[:comment])
+        expect(new_activity.contacts.map(&:address).map(&:email).sort).to eq(data[:contacts].split(';').sort)
+      end
+
+      it 'updates an existing activity by ID match' do
+        data = build :activity_csv_data, agency: agency.name, advertiser: advertiser.name, id: activity.id
+        expect do
+          expect(Activity.import(generate_csv(data), user)).to eq([])
+        end.not_to change(Activity, :count)
+        activity.reload
+
+        expect(activity.id).to eq(data[:id])
+        expect(activity.happened_at).to eq(data[:date])
+        expect(activity.creator.email).to eq(data[:creator])
+        expect(activity.updator.email).to eq(data[:creator])
+        expect(activity.user.email).to eq(data[:creator])
+        expect(activity.client.name).to eq(data[:advertiser])
+        expect(activity.agency.name).to eq(data[:agency])
+        expect(activity.deal.name).to eq(data[:deal])
+        expect(activity.activity_type_name).to eq(data[:type])
+        expect(activity.comment).to eq(data[:comment])
+      end
+
+      it 'adds new contacts to the existing ones' do
+        activity_contacts = activity.contacts.map(&:address).map(&:email)
+        data = build :activity_csv_data, agency: agency.name, advertiser: advertiser.name, id: activity.id
+        Activity.import(generate_csv(data), user)
+        activity.reload
+
+        expected_contacts = (activity_contacts + data[:contacts].split(';')).sort
+        expect(activity.contacts.map(&:address).map(&:email).sort).to eq(expected_contacts)
+      end
+
+      context 'invalid data' do
+        let!(:duplicate_advertiser) { create :client, client_type_id: advertiser_type_id(user.company), company: company }
+        let!(:duplicate_advertiser2) { create :client, client_type_id: advertiser_type_id(user.company), company: company, name: duplicate_advertiser.name }
+        let!(:duplicate_agency) { create :client, client_type_id: agency_type_id(user.company), company: company }
+        let!(:duplicate_agency2) { create :client, client_type_id: agency_type_id(user.company), company: company, name: duplicate_agency.name }
+        let!(:duplicate_deal) { create :deal, company: company }
+        let!(:duplicate_deal2) { create :deal, company: company, name: duplicate_deal.name }
+
+        it 'requires date to be present' do
+          data = build :activity_csv_data, date: nil
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ['Date is empty']}])
+        end
+
+        it 'requires date to be valid' do
+          data = build :activity_csv_data, date: 'zzz'
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ['Date must be a valid datetime']}])
+        end
+
+        it 'requires creator column to be present' do
+          data = build :activity_csv_data
+          data[:creator] = nil
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ['Creator is empty']}])
+        end
+
+        it 'requires creator user to exist' do
+          data = build :activity_csv_data, creator: 'N/A'
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["User #{data[:creator]} could not be found"]}])
+        end
+
+        it 'requires advertiser to exist' do
+          data = build :activity_csv_data, advertiser: 'N/A'
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["Advertiser #{data[:advertiser]} could not be found"]}])
+        end
+
+        it 'requires advertiser to match no more than 1 client' do
+          data = build :activity_csv_data, advertiser: duplicate_advertiser2.name
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["Advertiser #{data[:advertiser]} matched more than one client record"]}])
+        end
+
+        it 'requires agency to exist' do
+          data = build :activity_csv_data, agency: 'N/A'
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["Agency #{data[:agency]} could not be found"]}])
+        end
+
+        it 'requires agency to match no more than 1 record' do
+          data = build :activity_csv_data, agency: duplicate_agency2.name
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["Agency #{data[:agency]} matched more than one client record"]}]) 
+        end
+
+        it 'requires deal to exist' do
+          data = build :activity_csv_data, deal: 'N/A'
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["Deal #{data[:deal]} could not be found"]}])
+        end
+
+        it 'requires deal to match no more than 1 record' do
+          data = build :activity_csv_data, deal: duplicate_deal2.name
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["Deal #{data[:deal]} matched more than one deal record"]}])
+        end
+
+        it 'requires meeting type to be present' do
+          data = build :activity_csv_data
+          data[:type] = nil
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ['Activity type is empty']}])
+        end
+
+        it 'requires activity type to exist' do
+          data = build :activity_csv_data, type: 'N/A'
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["Activity type #{data[:type]} could not be found"]}])
+        end
+
+        it 'requires contact to exist' do
+          data = build :activity_csv_data, contacts: 'N/A'
+          expect(
+            Activity.import(generate_csv(data), user)
+          ).to eq([{row: 1, message: ["Activity contact #{data[:contacts]} could not be found in the contacts list"]}])
+        end
+      end
+    end
+  end
+
+  def generate_csv(data)
+    CSV.generate do |csv|
+      csv << data.keys
+      csv << data.values
+    end
   end
 end
