1ba464bb272c382aec763ab6774506a26384dc7c
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2016-12-23 11:50:06 +0200

    Updated column type for Revenue Fact table

--- db/migrate/20161221080554_change_data_type_for_revenue_amount.rb
+++ db/migrate/20161221080554_change_data_type_for_revenue_amount.rb
@@ -1,5 +1,9 @@
 class ChangeDataTypeForRevenueAmount < ActiveRecord::Migration
-  def change
+  def up
     change_column :account_revenue_facts, :revenue_amount, :decimal, precision: 9, scale: 2
+  end
+
+  def down
+    change_column :account_revenue_facts, :revenue_amount, :integer
   end
 end
