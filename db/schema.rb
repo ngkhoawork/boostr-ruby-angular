f6a7d3d1835f766d55728d1648734a69be05d056
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2017-01-31 01:08:18 +0200

    Updated existing TempIo local budgets to be equal to usd budget

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128165038) do
+ActiveRecord::Schema.define(version: 20170130230423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
