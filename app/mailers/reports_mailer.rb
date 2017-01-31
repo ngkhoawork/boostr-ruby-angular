45f52931e99a1d4cb17074568a25186f6f0c814b
baramik <baramik1989@gmail.com>
2017-01-31 16:00:52 +0200

    Change  email from to booster

--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -1,4 +1,6 @@
 class ReportsMailer < ApplicationMailer
+  default from: 'boostr <boostr@boostrcrm.com>'
+
   def deals_daily_mail(recipients, company_id)
     report_data = DealReportService.new(target_date: Date.yesterday, company_id: company_id).generate_report
     @new_deals = report_data[:new_deals]
