1eeac83c4efaba1101afc7501a38e5108aa029c7
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-02-07 10:09:25 +0200

    Added contact object mapper

--- app/representers/operative/clients/single.rb
+++ app/representers/operative/clients/single.rb
@@ -6,9 +6,9 @@ class Operative::Clients::Single < API::Single
   property :external_id, as: :externalID, exec_context: :decorator
   property :industry, exec_context: :decorator
   property :type, as: :accountType, exec_context: :decorator
-  property :address_line_1, as: :addressLine1, exec_context: :decorator
-  property :address_line_2, as: :addressLine2, exec_context: :decorator
   property :parent_account, as: :parentAccount, exec_context: :decorator
+  property :street1, as: :addressLine1
+  property :street2, as: :addressLine2
 
   def external_id
     represented.id
@@ -20,14 +20,6 @@ class Operative::Clients::Single < API::Single
 
   def industry
     represented.category_name
-  end
-
-  def address_line_1
-    represented.street1
-  end
-
-  def address_line_2
-    represented.street2
   end
 
   def parent_account
