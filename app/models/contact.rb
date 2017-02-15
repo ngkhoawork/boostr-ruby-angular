0607eba877fed3815d424c40a60069c167868257
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-02-15 15:26:11 +0200

    Added integration model for storing integrations data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,6 +9,7 @@ class Contact < ActiveRecord::Base
   has_many :deal_contacts, dependent: :destroy
   has_many :reminders, as: :remindable, dependent: :destroy
   has_one :address, as: :addressable
+  has_many :integrations, as: :integratable
 
   has_and_belongs_to_many :activities, after_add: :update_activity_updated_at
 
