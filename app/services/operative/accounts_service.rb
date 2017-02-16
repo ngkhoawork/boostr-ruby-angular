df19f676c65bd96b39b7359ff9a438ec59672207
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-02-16 14:41:26 +0200

    Added service for send accounts

---
+++ app/services/operative/accounts_service.rb
@@ -0,0 +1,53 @@
+class Operative::AccountsService
+  attr_reader :account, :mapped_object
+
+  def initialize(account)
+    @account = account
+  end
+
+  def perform
+    send_account if account
+  end
+
+  private
+
+  def send_account
+    account_integration_blank? ? create_account_and_integration_object : update_account
+  end
+
+  def mapped_object
+    @_mapped_object ||= Operative::Clients::Single.new(account).to_hash
+  end
+
+  def v1_api_client
+    Operative::V1::Client.new
+  end
+
+  def create_account
+    v1_api_client.create_account(params: mapped_object)
+  end
+
+  def create_account_and_integration_object
+    account.integrations.create!(external_id: external_id_from_response, external_type: Integration::OPERATIVE)
+  end
+
+  def external_id_from_response
+    Operative::XmlParserService.new(create_account, element: 'accountId').perform
+  end
+
+  def update_account
+    v1_api_client.update_account(params: mapped_object, id: account_external_id)
+  end
+
+  def account_operative_integration
+    @_account_operative_integration ||= account.integrations.operative
+  end
+
+  def account_integration_blank?
+    account_operative_integration.blank?
+  end
+
+  def account_external_id
+    account_operative_integration.external_id
+  end
+end
