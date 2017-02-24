bd4357e6cbfd47ad366bdcbfa5546914cc787257
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2017-02-24 17:22:25 +0200

    Optimized and fixed account export. Updated activity export

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name { FFaker::Company.name }
     website { FFaker::Internet.http_url }
     address
+    association :parent_client, factory: :parent_client
 
     before(:create) do |item|
       item.company = Company.first
@@ -10,6 +11,10 @@ FactoryGirl.define do
       if item.client_type_id.nil?
         item.client_type_id = Company.first.fields.find_by(name: 'Client Type').options.ids.sample
       end
+    end
+
+    factory :parent_client do
+      parent_client nil
     end
   end
 end
