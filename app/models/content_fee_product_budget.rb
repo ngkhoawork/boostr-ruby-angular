707bebbb3d9fc7730bab959ad8feb8d12deec555
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2017-02-27 18:43:41 +0200

    Changed daily budget calc on content fee to fix dates mismatch

--- app/models/content_fee_product_budget.rb
+++ app/models/content_fee_product_budget.rb
@@ -1,9 +1,14 @@
 class ContentFeeProductBudget < ActiveRecord::Base
   belongs_to :content_fee
+  delegate :io, to: :content_fee
 
   scope :for_time_period, -> (start_date, end_date) { where('content_fee_product_budgets.start_date <= ? AND content_fee_product_budgets.end_date >= ?', end_date, start_date) }
 
   def daily_budget
     budget.to_f / (end_date - start_date + 1).to_i
+  end
+
+  def corrected_daily_budget(io_start_date, io_end_date)
+    budget.to_f / ([io_end_date, end_date].min - [io_start_date, start_date].max + 1).to_i
   end
 end
