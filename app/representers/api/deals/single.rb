90c31f968349320ad4af593ded3cbf53236463ad
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2017-03-06 23:33:13 +0200

    Updated changed stage pipeline report

--- app/representers/api/deals/single.rb
+++ app/representers/api/deals/single.rb
@@ -1,8 +1,8 @@
 class API::Deals::Single < API::Single
   properties :id, :name, :advertiser_name, :start_date, :stage_name, :budget
-  property :deal_stage_log_previous_stage, exec_context: :decorator
+  property :previous_stage, exec_context: :decorator
 
-  def deal_stage_log_previous_stage
-    represented.deal_stage_logs.try(:last).try(:previous_stage).try(:name) || ''
+  def previous_stage
+    represented.try(:previous_stage).try(:name) || ''
   end
 end
