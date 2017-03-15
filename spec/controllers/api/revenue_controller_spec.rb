4f5c65c5882067fb86a40716aaaf9e162e3bbabc
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-03-15 14:53:10 +0200

    Fixed failed specs

--- spec/controllers/api/revenue_controller_spec.rb
+++ spec/controllers/api/revenue_controller_spec.rb
@@ -39,31 +39,34 @@ RSpec.describe Api::RevenueController, type: :controller do
 
     context 'team' do
       before do
-        create(:io_member, user: another_user, io: io_for_another_user, share: 80)
-        2.times { create :content_fee, io: io_for_another_user, product: product, budget: 10_000 }
-
         create(:io_member, user: user, io: io, share: 100)
         2.times { create :content_fee, io: io, product: product, budget: 20_000 }
+
+        create(:io_member, user: another_user, io: io_for_another_user, share: 80)
+        2.times { create :content_fee, io: io_for_another_user, product: product, budget: 10_000 }
       end
 
       it 'has proper revenue data' do
         get :index, format: :json, time_period_id: time_period.id, team_id: team.id.to_s
-        response_json = JSON.parse(response.body)
+        response_json = response_json(response.body)
 
-        expect(response_json[0]['name']).to eq(io.name)
-        expect(response_json[0]['advertiser']).to eq(advertiser.name)
-        expect(response_json[0]['budget']).to eq('40000.0')
-        expect(response_json[0]['sum_period_budget']).to eq(40000.0)
-
-        expect(response_json[1]['name']).to eq(io_for_another_user.name)
-        expect(response_json[1]['advertiser']).to eq(advertiser.name)
-        expect(response_json[1]['budget']).to eq('20000.0')
-        expect(response_json[1]['sum_period_budget']).to eq(20000.0)
+        expect(select_values(response_json, 'name')).to include(io.name && io_for_another_user.name)
+        expect(select_values(response_json, 'advertiser')).to include(advertiser.name)
+        expect(select_values(response_json, 'budget')).to include('40000.0' && '20000.0')
+        expect(select_values(response_json, 'sum_period_budget')).to include(40000.0 && 20000.0)
       end
     end
   end
 
   private
+
+  def response_json(body)
+    @_response_json ||= JSON.parse(body)
+  end
+
+  def select_values(json, key)
+    json.inject([]) { |values, obj| values << obj[key] }
+  end
 
   def time_period
     @_time_period ||= create :time_period, start_date: '2017-01-01', end_date: '2017-03-31'
