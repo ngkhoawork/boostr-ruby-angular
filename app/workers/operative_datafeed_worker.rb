e86e599fd5e2f99c5711953015d7e4c9f6cba668
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2017-03-24 00:52:51 +0200

    Added clockwork for Datafeed

--- app/workers/operative_datafeed_worker.rb
+++ app/workers/operative_datafeed_worker.rb
@@ -2,7 +2,7 @@ class OperativeDatafeedWorker < BaseWorker
   def perform
     datafeed_configs = ApiConfiguration.where(integration_type: "Operative Datafeed")
     datafeed_configs.each do |api_config|
-      Operative::DatafeedService.new(api_config, Date.today).perform
+      Operative::DatafeedService.new(api_config, Date.today - 1.day).perform
     end
   end
 end
