e452159eeb8c27ad15a42942b2c4b6837663cf30
baramik <baramik1989@gmail.com>
2017-04-04 20:41:52 +0300

    Return 422 status in case of wrong request to operative

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -253,7 +253,8 @@ class Api::DealsController < ApplicationController
       OperativeIntegrationWorker.perform_async(deal.id)
       render json: { message: 'deal was sent to operative' }
     else
-      render json: { message: 'cannot send this deal to operative please recheck a deal and try again later' }
+      render json: { message: 'cannot send this deal to operative please recheck a deal and try again later' },
+             status: :unprocessable_entity
     end
   end
 
