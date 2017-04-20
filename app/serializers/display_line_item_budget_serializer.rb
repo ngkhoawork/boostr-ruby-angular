e5462fc1dc718b5568c10516cba401d57fffaeca
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-04-20 11:56:56 +0300

    Added display line item id to serializer

--- app/serializers/display_line_item_budget_serializer.rb
+++ app/serializers/display_line_item_budget_serializer.rb
@@ -1,5 +1,5 @@
 class DisplayLineItemBudgetSerializer < ActiveModel::Serializer
-  attributes :id, :budget_loc, :month
+  attributes :id, :display_line_item_id, :budget_loc, :month
 
   def month
     object.start_date.strftime('%b %Y')
@@ -7,5 +7,9 @@ class DisplayLineItemBudgetSerializer < ActiveModel::Serializer
 
   def budget_loc
     object.budget_loc.to_i
+  end
+
+  def display_line_item_id
+    object.display_line_item.id
   end
 end
