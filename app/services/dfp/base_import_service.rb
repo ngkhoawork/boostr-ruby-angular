75cb14586a99667c4da82cd536b6724b5679fdad
baramik <baramik1989@gmail.com>
2017-04-28 16:25:46 +0300

    remove breakpoint

--- app/services/dfp/base_import_service.rb
+++ app/services/dfp/base_import_service.rb
@@ -39,7 +39,6 @@ class DFP::BaseImportService
           object_from_csv.perform
           import_log.count_imported
         rescue Exception => e
-          byebug
           import_log.count_failed
           import_log.log_error ['Internal Server Error', row.to_h.compact.to_s]
           next
