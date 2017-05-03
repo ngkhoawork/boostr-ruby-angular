e234bb8aca88ed4b01dbe04432644fcfe5360e9e
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2017-05-03 17:39:14 +0300

    Removed focus from spec

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -307,7 +307,7 @@ RSpec.describe Deal, type: :model do
     end
   end
 
-  describe '#import', focus: true do
+  describe '#import' do
     let!(:user) { create :user }
     let!(:another_user) { create :user }
     let!(:company) { user.company }
