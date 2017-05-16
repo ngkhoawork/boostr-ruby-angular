dabd24e1a074fb15bace76cc88f833235cf2bce5
baramik <baramik1989@gmail.com>
2017-05-16 23:46:56 +0300

    Remove adjustments from monthly reports

--- app/services/dfp/monthly_import_service.rb
+++ app/services/dfp/monthly_import_service.rb
@@ -12,7 +12,7 @@ module DFP
         clicks: row[:columntotal_line_item_level_clicks],
         ctr: row[:columntotal_line_item_level_ctr],
         impressions: row[:columntotal_line_item_level_impressions].to_i,
-        budget_loc: adjustment_service.perform(row[:columntotal_line_item_level_impressions].to_i),
+        budget_loc: row[:columntotal_line_item_level_impressions].to_i,
         video_avg_view_rate: row[:columnvideo_viewership_average_view_rate],
         video_completion_rate: row[:columnvideo_viewership_completion_rate],
         company_id: company_id
