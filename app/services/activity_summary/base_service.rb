0e5c3338920ba938540520273312d8f800d49b3c
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-05-30 16:35:57 +0300

    Apply filtering by user type

--- app/services/activity_summary/base_service.rb
+++ app/services/activity_summary/base_service.rb
@@ -12,34 +12,11 @@ class ActivitySummary::BaseService
     @_company ||= user.company
   end
 
-  def team
-    @_team ||= company.teams.find(params[:team_id])
-  end
-
-  def time_period
-    @_time_period ||= company.time_periods.find(params[:time_period_id])
-  end
-
   def start_date
     @_start_date ||= (params[:start_date] ? Date.parse(params[:start_date]) : (Time.now.end_of_day - 30.days))
   end
 
   def end_date
     @_end_date ||= (params[:end_date] ? Date.parse(params[:end_date]).end_of_day : Time.now.end_of_day)
-  end
-
-  def all_active_sales_users
-    company.teams.reduce([]) do |all_members, team|
-      all_members << team.all_sales_reps.select(&:is_active)
-    end
-  end
-
-  def all_team_sales_reps
-    @_all_team_sales_reps ||=
-      if params[:team_id] == 'all'
-        all_active_sales_users.flatten.uniq.sort_by(&:first_name)
-      else
-        team.all_sales_reps.select(&:is_active).sort_by(&:first_name).uniq
-      end
   end
 end
--- app/services/activity_summary/user_service.rb
+++ app/services/activity_summary/user_service.rb
@@ -3,10 +3,25 @@ class ActivitySummary::UserService < ActivitySummary::BaseService
     { user_activities: user_activity_reports, total_activity_report: total_activity_report }
   end
 
+  def perform_csv_service
+    Csv::ActivitySummaryService.new(
+      users,
+      activity_summary_user_csv_options
+    ).perform
+  end
+
   private
 
+  def activity_summary_user_csv_options
+    {
+      company: company,
+      start_date: start_date,
+      end_date: end_date
+    }
+  end
+
   def user_activity_reports
-    all_team_sales_reps.reduce([]) do |activity_report, user|
+    users.reduce([]) do |activity_report, user|
       user_activities = grouped_by_activity_types_for(user)
         .map { |activity| { user_id: user.id, username: user.name, activity.name => activity.count } }
         .reduce({}, :merge)
@@ -20,7 +35,7 @@ class ActivitySummary::UserService < ActivitySummary::BaseService
   end
 
   def total_activity_report
-    total_activities = grouped_by_activity_types_for(all_team_sales_reps.map(&:id))
+    total_activities = grouped_by_activity_types_for(users.map(&:id))
       .map { |activity| { activity.name => activity.count } }
 
     total_activities = total_activities.reduce({}, :merge)
@@ -35,5 +50,40 @@ class ActivitySummary::UserService < ActivitySummary::BaseService
       .for_time_period(start_date, end_date)
       .by_user(user)
       .group_by_activity_types_name
+  end
+
+  def all_active_sales_users
+    company.teams.reduce([]) do |all_members, team|
+      all_members << team.all_sales_reps.select(&:is_active)
+    end
+  end
+
+  def all_team_sales_reps
+    @_all_team_sales_reps ||=
+      if params[:team_id] == 'all'
+        all_active_sales_users.flatten.uniq.sort_by(&:first_name)
+      else
+        team.all_sales_reps.select(&:is_active).sort_by(&:first_name).uniq
+      end
+  end
+
+  def team
+    @_team ||= company.teams.find(params[:team_id])
+  end
+
+  def user_type
+    @_user_type ||= params[:user_type].to_i
+  end
+
+  def users
+    @_users ||= params[:user_type].present? ? by_user_type : without_default_and_fake_type
+  end
+
+  def by_user_type
+    all_team_sales_reps.select { |u| u.user_type.eql? user_type }
+  end
+
+  def without_default_and_fake_type
+    all_team_sales_reps.reject { |u| [FAKE_USER, DEFAULT].include?(u.user_type) }
   end
 end
