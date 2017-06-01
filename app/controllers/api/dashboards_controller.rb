47a93d8a6258408a485418b0bf293b9791a2aeba
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-06-01 16:19:32 +0300

    Updated quarter getting for stats

--- app/controllers/api/dashboards_controller.rb
+++ app/controllers/api/dashboards_controller.rb
@@ -22,40 +22,32 @@ class Api::DashboardsController < ApplicationController
   end
 
   def time_period
-    return @time_period if defined?(@time_period)
-
-    @time_period = company.time_periods.now
+    @_time_period ||= company.time_periods.current_quarter
   end
 
   def next_time_period
-    start_date = (Time.now.utc + 3.months).beginning_of_quarter
-    end_date = (Time.now.utc + 3.months).end_of_quarter.beginning_of_day
-    start_date..end_date
+    company.time_periods.all_quarter.find_by(start_date: time_period.end_date.next)
   end
 
   def forecast
-    return @forecast if defined?(@forecast)
-
     return nil unless time_period
 
-    if current_user.user_type == EXEC && user_quota_for_period(time_period.start_date, time_period.end_date) == 0
-      @forecast = Forecast.new(company, company_teams, time_period.start_date, time_period.end_date)
-    elsif current_user.leader?
-      @forecast = Forecast.new(company, current_user.teams, time_period.start_date, time_period.end_date)
-    else
-      @forecast = ForecastMember.new(current_user, time_period.start_date, time_period.end_date)
-    end
+    @_forecast ||= forecast_for(time_period)
   end
 
   def next_quarter_forecast
-    return @next_quarter_forecast if defined?(@next_quarter_forecast)
+    return nil unless time_period || next_time_period
 
-    if current_user.user_type == EXEC && user_quota_for_period(next_time_period.first, next_time_period.last) == 0
-      @next_quarter_forecast = Forecast.new(company, company_teams, next_time_period.first, next_time_period.last)
+    @_next_quarter_forecast ||= forecast_for(next_time_period)
+  end
+
+  def forecast_for(period)
+    if current_user.user_type.eql?(EXEC) && user_quota_for_period(period.start_date, period.end_date) == 0
+      Forecast.new(company, company_teams, period.start_date, period.end_date)
     elsif current_user.leader?
-      @next_quarter_forecast = Forecast.new(company, current_user.teams, next_time_period.first, next_time_period.last)
+      Forecast.new(company, current_user.teams, period.start_date, period.end_date)
     else
-      @next_quarter_forecast = ForecastMember.new(current_user, next_time_period.first, next_time_period.last)
+      ForecastMember.new(current_user, period.start_date, period.end_date)
     end
   end
 
@@ -64,28 +56,22 @@ class Api::DashboardsController < ApplicationController
   end
 
   def company_teams
-    @teams = company.teams.roots(true)
+    @_teams = company.teams.roots(true)
   end
 
   def deals
-    return @deals if defined?(@deals)
-
-    @deals = current_user.deals.open.order("start_date asc")
+    @_deals ||= current_user.deals.open.order('start_date')
   end
 
   def deal_search
-    return @deal_search if defined?(@deal_search)
-
-    @deal_search = current_user.deals.open.where('name ilike ?', "%#{params[:query]}%")
+    @_deal_search ||= current_user.deals.open.by_name(params[:query])
   end
 
   def company
-    return @company if defined?(@company)
-    @company = current_user.company
+    @_company ||= current_user.company
   end
 
   def serialized_deals
     ActiveModel::ArraySerializer.new(deals, each_serializer: DashboardDealSerializer)
   end
-
 end
