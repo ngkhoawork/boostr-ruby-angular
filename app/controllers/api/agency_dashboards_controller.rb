791dbeacc3b6bb27b1fe6bbb45f3c0936a5c2a1a
baramik <baramik1989@gmail.com>
2017-06-22 15:56:24 +0200

    Add total calculations for Spent By Product dashboard part

--- app/controllers/api/agency_dashboards_controller.rb
+++ app/controllers/api/agency_dashboards_controller.rb
@@ -1,11 +1,10 @@
 class Api::AgencyDashboardsController < ApplicationController
 
   def spend_by_product
-    filtered_pipelines = FactTables::AccountProductPipelineFacts::FilteredQuery.new(filter_params).call
-    filtered_revenues = FactTables::AccountProductRevenueFacts::FilteredQuery.new(filter_params).call
-    pipeline_sums = FactTables::AccountProductPipelineFacts::PipelineSumQuery.new(filtered_pipelines).call
-    revenue_sums = FactTables::AccountProductRevenueFacts::RevenueSumQuery.new(filtered_revenues).call
-    render json: { revenue: revenue_sums, pipeline: pipeline_sums }
+    render json: { revenue: revenue_sums_by_products,
+                   pipeline: pipeline_sums_by_products,
+                   pipeline_totals: pipeline_total_by_time_dim,
+                   revenue_totals: revenue_total_by_time_dim }
   end
 
   def by_agency
@@ -13,6 +12,34 @@ class Api::AgencyDashboardsController < ApplicationController
   end
 
   private
+
+  def revenue_total_by_time_dim
+    revenue_sums_by_products.unscope(:group, :order, :select)
+                            .group('time_dimensions.start_date, time_dimensions.end_date')
+                            .select('time_dimensions.start_date, time_dimensions.end_date, sum(revenue_amount) as revenue_sum')
+  end
+
+  def pipeline_total_by_time_dim
+    pipeline_sums_by_products.unscope(:group, :order, :select)
+                             .group('time_dimensions.start_date, time_dimensions.end_date')
+                             .select('time_dimensions.start_date, time_dimensions.end_date, sum(weighted_amount) as pipeline_sum')
+  end
+
+  def revenue_sums_by_products
+    FactTables::AccountProductRevenueFacts::RevenueSumQuery.new(filtered_revenues_by_products).call
+  end
+
+  def pipeline_sums_by_products
+    FactTables::AccountProductPipelineFacts::PipelineSumQuery.new(filtered_pipelines_by_products).call
+  end
+
+  def filtered_pipelines_by_products
+    FactTables::AccountProductPipelineFacts::FilteredQuery.new(filter_params).call
+  end
+
+  def filtered_revenues_by_products
+    FactTables::AccountProductRevenueFacts::FilteredQuery.new(filter_params).call
+  end
 
   def filter_params
     params.permit(:start_date, :end_date, :holding_company_name, :account_name, :company_id)
