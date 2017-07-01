3822171c25323629c3673fa19021e4b3eea4bcc3
Dmitriy Shirshov <shirshov.dmitriy@gmail.com>
2017-07-01 23:41:46 +0300

    Changed name of asana config on testing

--- config/initializers/asana_connect.rb
+++ config/initializers/asana_connect.rb
@@ -1,3 +1,3 @@
 asana_connect_file = YAML.load_file("#{Rails.root}/config/asana_connect.yml") || {}
-ASANA_CONNECT = OpenStruct.new(asana_connect_file['asana'][Rails.env])
+ASANA_CONNECT = OpenStruct.new(asana_connect_file['asana'][ENV['ASANA_CONNECT_ENV']])
 ASANA_CONNECT.freeze
