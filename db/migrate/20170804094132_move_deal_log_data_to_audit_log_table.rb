5bdd219b2defee4086ae90544078c7adf9abc210
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-08-14 11:21:32 +0300

    Changed type of changed amount field

--- db/migrate/20170804094132_move_deal_log_data_to_audit_log_table.rb
+++ db/migrate/20170804094132_move_deal_log_data_to_audit_log_table.rb
@@ -6,7 +6,7 @@ class MoveDealLogDataToAuditLogTable < ActiveRecord::Migration
         auditable_id: deal_log.deal_id,
         type_of_change: 'Budget Change',
         company: Deal.with_deleted.find(deal_log.deal_id).company,
-        changed_amount: deal_log.budget_change.to_i,
+        changed_amount: deal_log.budget_change,
         created_at: deal_log.created_at
       )
     end
