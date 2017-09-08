60c009f7384744afb46f7ec93db426b9d5f6677e
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-09-08 12:54:24 +0300

    Changed tests config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,11 +77,12 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before :each do
-    if Capybara.current_driver == :rack_test
+    if DatabaseCleaner.strategy.eql?(example.metadata[:js])
       DatabaseCleaner.strategy = :transaction
     else
       DatabaseCleaner.strategy = :truncation
     end
+
     DatabaseCleaner.start
     FactoryGirl.create(:company)
   end
@@ -90,18 +91,5 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
