1a518887e800623739635656b6ba495be40617b1
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-09-08 13:06:57 +0300

    Returned previous config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,7 +77,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before :each do
-    if DatabaseCleaner.strategy.eql?(example.metadata[:js])
+    if Capybara.current_driver == :rack_test
       DatabaseCleaner.strategy = :transaction
     else
       DatabaseCleaner.strategy = :truncation
