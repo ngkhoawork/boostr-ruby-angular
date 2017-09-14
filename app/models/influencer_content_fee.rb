366df3a186d2a1ba32af4dc7468a655a52c70f41
Zhang <zhang@boostrcrm.com>
2017-09-14 07:04:32 -0700

    added rspec for creating influencer_content_fee

--- app/models/influencer_content_fee.rb
+++ app/models/influencer_content_fee.rb
@@ -18,7 +18,7 @@ class InfluencerContentFee < ActiveRecord::Base
   end
 
   def update_net
-    if influencer.agreement.present?
+    if influencer.present? && influencer.agreement.present?
       fee_amount = self.fee_amount
       fee_amount_loc = (fee_amount || 0) / self.exchange_rate if self.exchange_rate && self.fee_type == 'flat'
       if self.fee_type && self.fee_type == 'flat'
--- spec/factories/influencer_content_fees.rb
+++ spec/factories/influencer_content_fees.rb
@@ -1,13 +1,12 @@
 FactoryGirl.define do
   factory :influencer_content_fee do
     influencer nil
-content_fee nil
-fee_type "MyString"
-curr_cd "MyString"
-gross_amount "9.99"
-gross_amount_loc "9.99"
-net "9.99"
-asset "MyText"
+		content_fee nil
+		fee_type "MyString"
+		curr_cd "MyString"
+		gross_amount "9.99"
+		gross_amount_loc "9.99"
+		net "9.99"
+		asset "MyText"
   end
-
 end
