ed784c609069dd6e6ee6bb7955cf7aa52aa39c77
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-09-20 00:20:19 +0800

    fix divide by zero in initiative

--- app/serializers/initiatives/smart_report_serializer.rb
+++ app/serializers/initiatives/smart_report_serializer.rb
@@ -14,6 +14,7 @@ class Initiatives::SmartReportSerializer < ActiveModel::Serializer
   end
 
   def goal_probability
+    return 'N/A' if !goal || goal == 0
     (pipeline + won) / (goal / 100)
   end
 
