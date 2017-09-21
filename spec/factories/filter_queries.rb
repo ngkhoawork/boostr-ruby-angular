5be26a3060d712e0c1d05de389ac80282edf62ff
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-09-21 16:07:47 +0300

    Added ability to add default query

--- spec/factories/filter_queries.rb
+++ spec/factories/filter_queries.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :filter_query do
     name 'MyString'
-    query_type 'Pipeline Summary'
-    filter_params { { user_id: 1, team_id: 2 } }
+    query_type 'pipeline_summary_report'
+    filter_params '{user_id: 1, team_id: 2}'
   end
 end
