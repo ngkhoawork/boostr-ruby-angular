c7997ef0d2e78ceb95e4d61a3ef9cb0fcc865a93
Dmytro Shyrshov <shirshov.dmitriy@gmail.com>
2017-10-20 18:03:23 +0300

    Fixed default value for existing records

--- db/migrate/20171020091546_add_exclude_child_line_items_to_datafeed_configuration_details.rb
+++ db/migrate/20171020091546_add_exclude_child_line_items_to_datafeed_configuration_details.rb
@@ -1,5 +1,5 @@
 class AddExcludeChildLineItemsToDatafeedConfigurationDetails < ActiveRecord::Migration
   def change
-    add_column :datafeed_configuration_details, :exclude_child_line_items, :boolean
+    add_column :datafeed_configuration_details, :exclude_child_line_items, :boolean, default: false
   end
 end
