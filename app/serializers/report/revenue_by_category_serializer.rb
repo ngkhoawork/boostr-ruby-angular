d2fca4650c188e752b862e6334a16cac57b5ea5e
Yaroslav Goldyschuk <goildiaroslav@gmail.com>
2017-10-24 14:28:10 +0300

    Improve RevenueByCategory serializer

--- app/serializers/report/revenue_by_category_serializer.rb
+++ app/serializers/report/revenue_by_category_serializer.rb
@@ -1,15 +1,39 @@
 class Report::RevenueByCategorySerializer < ActiveModel::Serializer
-  ATTRIBUTES = %i(category_id region_id segment_id year revenues total_revenue).freeze
 
-  attributes *ATTRIBUTES
+  attributes :category_id, :year, :revenues, :total_revenue, :region_id, :segment_id
 
   def attributes(*args)
     super(*args).compact
   end
 
-  ATTRIBUTES.each do |attr|
-    define_method(attr) do
-      object.try(attr)
-    end
+  def category_id
+    object.category_id
+  end
+
+  def year
+    object.year.to_i
+  end
+
+  def revenues
+    revenues_to_f!
+    object.revenues
+  end
+
+  def total_revenue
+    object.total_revenue.to_f
+  end
+
+  def region_id
+    object.try(:region_id)
+  end
+
+  def segment_id
+    object.try(:segment_id)
+  end
+
+  private
+
+  def revenues_to_f!
+    object.revenues.each { |k, v| object.revenues[k] = v.to_f  }
   end
 end
