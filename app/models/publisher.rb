61cbb5aa0a59181cf440bd5390e4d02e78addf7f
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-11-13 23:11:33 +0200

    Added Custom Values to Publisher

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -2,11 +2,12 @@ class Publisher < ActiveRecord::Base
   acts_as_paranoid
 
   has_one :address, as: :addressable, dependent: :destroy
-  has_many :activities
+  has_many :activities, dependent: :destroy
   has_many :contacts
   has_many :publisher_members, dependent: :destroy
   has_many :users, through: :publisher_members
   has_many :sales_stage, as: :sales_stageable
+  has_many :values, as: :subject
 
   belongs_to :clients
 
