13994a878876f7fbd54454d6b588e6239e061b02
Vitaleks-project <vitaliy.jorzh100592@gmail.com>
2017-11-15 12:00:29 +0200

    fix JS; add domain to assets

--- app/controllers/api/v2/gmail_extension_controller.rb
+++ app/controllers/api/v2/gmail_extension_controller.rb
@@ -1,5 +1,11 @@
 class Api::V2::GmailExtensionController < ApiController
   def index
-
+    if request.domain == 'localhost'
+      @domain = request.domain + ":#{request.port}"
+    else
+      @domain = request.domain
+    end
+    
+    response.headers["Content-Type"] = "application/html"
   end
 end
