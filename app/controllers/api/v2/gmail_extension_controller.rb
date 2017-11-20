a755abb21465a0aa82ca9532ca4a915cf0d52987
Sergey Guzhvay <feidos.gs@gmail.com>
2017-11-20 18:33:16 +0200

    changing gmail_extension api response

--- app/controllers/api/v2/gmail_extension_controller.rb
+++ app/controllers/api/v2/gmail_extension_controller.rb
@@ -1,12 +1,5 @@
 class Api::V2::GmailExtensionController < ApiController
   def index
-    @cert = request.ssl? ? "https://" : "http://"
-    if request.domain == 'localhost'
-      @domain = @cert + request.domain + ":#{request.port}"
-    else
-      @domain = @cert + request.domain
-    end
-    
     response.headers["Content-Type"] = "text/html; charset=utf-8"
     response.headers["Content-Security-Policy"] = ""
     response.headers.delete("X-Frame-Options")
