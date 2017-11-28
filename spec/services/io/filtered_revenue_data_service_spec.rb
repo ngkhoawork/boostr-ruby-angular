5fc1fa0b1aef381faaf9e783a45c430d465298ed
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-11-29 04:28:44 +0900

    filtered revenue data service unit test

---
+++ spec/services/io/filtered_revenue_data_service_spec.rb
@@ -0,0 +1,106 @@
+require 'rails_helper'
+
+describe Io::FilteredRevenueDataService do
+  before do
+    io(start_date: '2015-04-01', end_date: '2015-06-30')
+    content_fee(io: io, product: products[0], budget: 10000, budget_loc: 10000)
+    content_fee2(io: io, product: products[1], budget: 5000, budget_loc: 5000)
+    io_member(io: io, user: user, share: 60, from_date: '2015-04-01', to_date: '2015-06-30')
+    io_member2(io: io, user: user2, share: 40, from_date: '2015-04-01', to_date: '2015-06-30')
+  end
+
+  it 'return correct filtered revenue data service' do
+    expect(
+      filtered_revenue_data_service(
+        io,
+        time_period.start_date,
+        time_period.end_date,
+        [io_member.user_id],
+        nil
+      ).perform
+    ).to eq([15000, 9000])
+  end
+  
+  it 'return correct filtered revenue data service for one product & one member' do
+    expect(
+      filtered_revenue_data_service(
+        io,
+        time_period.start_date,
+        time_period.end_date,
+        [io_member.user_id],
+        [products[0].id]
+      ).perform
+    ).to eq([10000, 6000])
+  end
+  
+  it 'return correct filtered revenue data service for multiple products & one member' do
+    expect(
+      filtered_revenue_data_service(
+        io,
+        time_period.start_date,
+        time_period.end_date,
+        [io_member.user_id],
+        products.collect(&:id)
+      ).perform
+    ).to eq([15000, 9000])
+  end
+  
+  it 'return correct filtered revenue data service for multiple products & multiple members' do
+    expect(
+      filtered_revenue_data_service(
+        io,
+        time_period.start_date,
+        time_period.end_date,
+        [io_member.user_id, io_member2.user_id],
+        products.collect(&:id)
+      ).perform
+    ).to eq([15000, 15000])
+  end
+
+  private
+
+  def filtered_revenue_data_service(io, start_date, end_date, member_ids, product_ids)
+    @filtered_revenue_data_service ||= described_class.new(io, start_date, end_date, member_ids, product_ids)
+  end
+
+  def company
+    @_company ||= Company.first
+  end
+
+  def user
+    @_user ||= create :user, win_rate: 0.5, average_deal_size: 300
+  end
+  
+  def user2
+    @_user2 ||= create :user, win_rate: 0.5, average_deal_size: 300
+  end
+
+  def time_period
+    @_time_period ||= create :time_period, start_date: '2015-01-01', end_date: '2015-12-31', period_type: 'year'
+  end
+
+  def products
+    @_products ||= create_list :product, 2
+  end
+
+  def io(opts={})
+    opts[:company_id] = company.id
+    @_io ||= create :io, opts
+  end
+
+  def io_member(opts={})
+    @_io_member ||= create :io_member, opts
+  end
+
+  def io_member2(opts={})
+    @_io_member2 ||= create :io_member, opts
+  end
+
+  def content_fee(opts={})
+    @_content_fee ||= create :content_fee, opts
+  end
+
+  def content_fee2(opts={})
+    @_content_fee2 ||= create :content_fee, opts
+  end
+end
