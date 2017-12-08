ec47764ec9ad12b065e3803761ed647dd58e5364
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-12-08 12:52:05 +0200

    Added publisher custom fields

--- app/serializers/api/publishers/show_serializer.rb
+++ app/serializers/api/publishers/show_serializer.rb
@@ -1,5 +1,6 @@
 class Api::Publishers::ShowSerializer < Api::Publishers::Serializer
-  attributes :publisher_stage_id, :type_id, :fill_rate, :revenue_lifetime, :revenue_ytd, :contacts
+  attributes :publisher_stage_id, :type_id, :fill_rate, :revenue_lifetime, :revenue_ytd, :contacts,
+             :publisher_custom_field_obj
 
   def fill_rate
     daily_actuals_for_current_month.sum(:fill_rate) / daily_actuals_for_current_month.count rescue 0
@@ -17,6 +18,10 @@ class Api::Publishers::ShowSerializer < Api::Publishers::Serializer
     object.contacts.includes(:primary_client_contact, :address, :client).map do |contact|
       Api::Publishers::ContactsSerializer.new(contact).as_json
     end
+  end
+
+  def publisher_custom_field_obj
+    object.publisher_custom_field
   end
 
   has_one :publisher_custom_field
