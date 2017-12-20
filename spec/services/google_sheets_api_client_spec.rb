7c8ba30bd433fd8bb6447fc9959cb6318f9b4366
Dmitry Smirnov <dmitry.smirnov@onapp.com>
2017-12-20 16:07:43 +0200

    Add wrapper for google API client

---
+++ spec/services/google_sheets_api_client_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe GoogleSheetsApiClient, vcr: true do
+  describe '::add_row' do
+    let(:deal) { create :deal, id: 1000 }
+
+    it 'should add new row to spreadsheet' do
+      expect(described_class.add_row('1ADkamKx7LJB1SzXmCMy29Yw3ocA0VBeJ5mrV3ugz0Wg', deal)).to be true
+    end
+  end
+
+  describe '::update_row' do
+    let(:deal) { create :deal, id: 2000 }
+
+    before do
+      described_class.add_row('1ADkamKx7LJB1SzXmCMy29Yw3ocA0VBeJ5mrV3ugz0Wg', deal)
+    end
+
+    it 'should update existing row in spreadsheet' do
+      expect(described_class.update_row('1ADkamKx7LJB1SzXmCMy29Yw3ocA0VBeJ5mrV3ugz0Wg', deal)).to be true
+    end
+  end
+end
