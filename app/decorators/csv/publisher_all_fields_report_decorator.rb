a270ed062afd45a931c06bd094c681b6fd3232dd
Khoa Ngyuen <ngkhoawork@gmail.com>
2017-12-21 17:11:51 +0200

    Changed export publisher report

--- app/decorators/csv/publisher_all_fields_report_decorator.rb
+++ app/decorators/csv/publisher_all_fields_report_decorator.rb
@@ -15,57 +15,71 @@ class Csv::PublisherAllFieldsReportDecorator
 
   DELEGATE_TO_PUBLISHER_ATTRIBUTES.each do |method_name|
     define_method(method_name) do
-      @record.send(method_name)
+      record.send(method_name)
     end
   end
 
   def type
-    @record.type&.name
+    record.type&.name
   end
 
   def publisher_stage
-    @record.publisher_stage&.name
+    record.publisher_stage&.name
   end
 
   def client
-    @record.client&.name
+    record.client&.name
   end
 
   def created_at
-    @record.created_at.to_date
+    record.created_at.to_date
   end
 
   def fill_rate
     return '0%' if fill_rate_sum_for_previous_month.zero?
 
-    "#{(fill_rate_sum_for_previous_month / @record.daily_actuals_for_previous_month.size).round(1)}%"
+    "#{(fill_rate_sum_for_previous_month / record.daily_actuals_for_previous_month.size).round(1)}%"
   end
 
   def revenue_lifetime
-    "#{curr_symbol}#{@record.daily_actuals.to_a.sum(&:total_revenue)}"
+    return '$0' if sum_revenue_lifetime.zero?
+
+    "#{curr_symbol}#{sum_revenue_lifetime}"
   end
 
   def revenue_ytd
-    "#{curr_symbol}#{@record.daily_actuals_for_current_year.to_a.sum(&:total_revenue)}"
+    return '$0' if sum_revenue_ytd.zero?
+
+    "#{curr_symbol}#{sum_revenue_ytd}"
   end
 
   def last_export_date
-    @record.last_daily_actual&.created_at
+    record.last_daily_actual&.created_at
   end
 
   def custom_fields
-    return unless @record.publisher_custom_field
+    return unless record.publisher_custom_field
 
-    FlatPublisherCustomFieldSerializer.new(@record.publisher_custom_field).attributes
+    FlatPublisherCustomFieldSerializer.new(record.publisher_custom_field).attributes
   end
 
   private
 
+  attr_reader :record
+
   def fill_rate_sum_for_previous_month
-    @_fill_rate_sum ||= @record.daily_actuals_for_previous_month.to_a.sum(&:fill_rate)
+    @_fill_rate_sum ||= record.daily_actuals_for_previous_month.to_a.sum(&:fill_rate)
   end
 
   def curr_symbol
-    @record.daily_actuals.first&.currency&.curr_symbol
+    record.daily_actuals.first&.currency&.curr_symbol
+  end
+
+  def sum_revenue_lifetime
+    @_sum_revenue_lifetime ||= record.daily_actuals.to_a.sum(&:total_revenue)
+  end
+
+  def sum_revenue_ytd
+    @_sum_revenue_ytd ||= record.daily_actuals_for_current_year.to_a.sum(&:total_revenue)
   end
 end
