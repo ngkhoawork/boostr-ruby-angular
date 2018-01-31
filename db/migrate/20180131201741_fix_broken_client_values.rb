7ddcb41efe19b4e9f69b6cf7679d4e22acb06be2
Dmytro Shyrshov <shirshov.dmitriy@gmail.com>
2018-01-31 23:13:40 +0200

    Fixing migration

--- db/migrate/20180131201741_fix_broken_client_values.rb
+++ db/migrate/20180131201741_fix_broken_client_values.rb
@@ -1,6 +1,6 @@
 class FixBrokenClientValues < ActiveRecord::Migration
   def change
-    Company.each do |company|
+    Company.all.each do |company|
       field = Field.find_by(company_id: company.id, subject_type: 'Client', name: 'Client Type')
       oids = field.options.ids
       Value.where(company_id: company.id, option_id: oids).where.not(field_id: field.id).update_all(field_id: field.id)
