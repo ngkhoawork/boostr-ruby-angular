4cbf2bad428ce9b0b2e9921d99905e19af19e8cf
Dmitry Smirnov <dmitler@gmail.com>
2018-02-13 23:51:18 +0200

    Update DisplayLineItemBudgetSerializer#ad_server_budget fields type

--- app/serializers/dataexport/display_line_item_budget_serializer.rb
+++ app/serializers/dataexport/display_line_item_budget_serializer.rb
@@ -4,4 +4,8 @@ class Dataexport::DisplayLineItemBudgetSerializer < ActiveModel::Serializer
 
   attributes :id, :display_line_item_id, :budget_usd, :budget, :start_date, :end_date, :created,
              :last_updated, :manual_override, :ad_server_budget, :ad_server_quantity, :quantity
+
+  def ad_server_budget
+    object.ad_server_budget.to_f
+  end
 end
