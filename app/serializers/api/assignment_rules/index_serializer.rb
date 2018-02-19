562d958c7cac507c13bedade54a730a29b64221f
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-02-19 14:50:26 +0200

    Added position to assignment rules serializer

--- app/serializers/api/assignment_rules/index_serializer.rb
+++ app/serializers/api/assignment_rules/index_serializer.rb
@@ -1,5 +1,5 @@
 class Api::AssignmentRules::IndexSerializer < ActiveModel::Serializer
-  attributes :id, :name, :countries, :states, :users
+  attributes :id, :name, :countries, :states, :users, :position
 
   def users
     object.users.as_json(only: :id, methods: :name, override: true) rescue nil
