1e002243ed016d1f52a36a15aa14406b885cbe16
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-03-02 02:42:34 +0900

    remove product existence validation

--- app/models/quota.rb
+++ app/models/quota.rb
@@ -15,7 +15,6 @@ class Quota < ActiveRecord::Base
 
   validates :user_id, :time_period_id, :company_id, :value_type, presence: true
   validates_uniqueness_of :product, :scope => [:product_type, :time_period_id, :value_type, :user_id], message: 'has already been taken along with user, time period and type'
-  validate :validate_product_existence
 
   def as_json(options={})
     super(options.merge(
@@ -31,19 +30,5 @@ class Quota < ActiveRecord::Base
   def set_dates
     self.start_date ||= self.time_period.try(:start_date)
     self.end_date ||= self.time_period.try(:end_date)
-  end
-
-  private 
-
-  def validate_product_existence
-    if product_type == 'Product' && product.product_family.present?
-      quota = Quota.where(user_id: user_id, time_period_id: time_period_id, value_type: value_type, product_id: product.product_family_id, product_type: 'ProductFamily').first
-      errors.add(:base, "Product Family - #{product.product_family.name} has already taken") if quota.present?
-    elsif product_type == 'ProductFamily'
-      product.products.each do |p|
-        quota = Quota.where(user_id: user_id, time_period_id: time_period_id, value_type: value_type, product_id: p.id, product_type: 'Product').first
-        errors.add(:base, "Product - #{p.name} has already taken") if quota.present?
-      end
-    end
   end
 end
