9312bcd336681be19224581aa5436e93f5f36f7f
Zhang <zhang@boostrcrm.com>
2018-03-02 10:27:44 -0800

    updates on feedbacks

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,13 +1,12 @@
 class Cost < ActiveRecord::Base
-  belongs_to :company
   belongs_to :io
   belongs_to :product
-
   has_many :values, as: :subject
-
   has_many :cost_monthly_amounts, dependent: :destroy
 
   validate :active_exchange_rate
+
+  delegate :company, to: :io
 
   accepts_nested_attributes_for :cost_monthly_amounts
   accepts_nested_attributes_for :values
@@ -15,33 +14,25 @@ class Cost < ActiveRecord::Base
   scope :estimated, -> { where('is_estimated IS true') }
   scope :for_product_ids, -> (product_ids) { where("product_id in (?)", product_ids) }
 
-
   after_create do
-    generate_cost_monthly_amounts if self.cost_monthly_amounts.count == 0
+    generate_cost_monthly_amounts if cost_monthly_amounts.count == 0
   end
 
   after_destroy do
-    update_revenue_budget(self)
+    update_revenue_budget
   end
 
   after_update do
-    if cost_monthly_amounts.sum(:budget) != budget || cost_monthly_amounts.sum(:budget_loc) != budget_loc
-      if budget_changed? || budget_loc_changed?
-        Cost::AmountsUpdateService.new(self).perform
-        self.update(is_estimated: false)
-      else
-        self.update_budget
-      end
-    end
+    update_cost_or_monthly_budget
   end
 
   set_callback :save, :after, :update_revenue_fact_callback
 
   def update_revenue_fact_callback
-    update_revenue_budget(self) if budget_changed? || budget_loc_changed?
+    update_revenue_budget if budget_changed? || budget_loc_changed?
     if product_id_changed?
       if product_id_was.present?
-        old_product = Product.find_by_id(product_id_was)
+        old_product = Product.find(product_id_was)
         update_revenue_product(old_product) if old_product.present?
       end
       update_revenue_product(product)
@@ -49,29 +40,22 @@ class Cost < ActiveRecord::Base
   end
 
   def update_revenue_product(product)
-    io = self.io
     if io.present? && product.present?
-      company = io.company
-      time_periods = company.time_periods.where("end_date >= ? and start_date <= ?", io.start_date, io.end_date)
+      time_periods = company.time_periods.for_time_period(io.start_date, io.end_date)
       time_periods.each do |time_period|
         io.users.each do |user|
-          ForecastCostFactCalculator::Calculator.new(time_period, user, product)
-            .calculate()
+          ForecastCostFactCalculator::Calculator.new(time_period, user, product).calculate()
         end
       end
     end
   end
 
-  def update_revenue_budget(cost)
-    io = cost.io
-    product = cost.product
+  def update_revenue_budget
     if io.present? && product.present?
-      company = io.company
-      time_periods = company.time_periods.where("end_date >= ? and start_date <= ?", io.start_date, io.end_date)
+      time_periods = company.time_periods.for_time_period(io.start_date, io.end_date)
       time_periods.each do |time_period|
         io.users.each do |user|
-          ForecastCostFactCalculator::Calculator.new(time_period, user, product)
-            .calculate()
+          ForecastCostFactCalculator::Calculator.new(time_period, user, product).calculate()
         end
       end
     end
@@ -111,11 +95,26 @@ class Cost < ActiveRecord::Base
     Cost::AmountsGenerateService.new(self).perform
   end
 
+  def update_cost_monthly_amounts
+    Cost::AmountsUpdateService.new(self).perform
+  end
+
   def update_periods
     cost_monthly_amounts.each_with_index do |cost_monthly_amount, index|
       period = Date.new(*io.months[index])
       cost_monthly_amount.start_date = [period, io.start_date].max
       cost_monthly_amount.end_date = [period.end_of_month, io.end_date].min
+    end
+  end
+
+  def update_cost_or_monthly_budget
+    if cost_monthly_amounts.sum(:budget) != budget || cost_monthly_amounts.sum(:budget_loc) != budget_loc
+      if budget_changed? || budget_loc_changed?
+        update_cost_monthly_amounts
+        self.update(is_estimated: false)
+      else
+        self.update_budget
+      end
     end
   end
 end
