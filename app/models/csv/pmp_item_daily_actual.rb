eb95e24b4845363a1a150049ad639ed6f15f8ac4
Dmitry Smirnov <dmitler@gmail.com>
2018-03-16 15:06:17 +0200

    Refactor CSV importer for PmpItemDailyActuals

--- app/models/csv/pmp_item_daily_actual.rb
+++ app/models/csv/pmp_item_daily_actual.rb
@@ -1,216 +1,149 @@
 require 'chronic'
 
 class Csv::PmpItemDailyActual
-  include ActiveModel::Validations
+  ALLOWED_ATTRIBUTES = %i[date ad_unit ad_requests impressions win_rate price revenue revenue_loc
+                          pmp_item_id md5_signature ssp_advertiser imported advertiser_id].freeze
+  CSV_OPTIONS = { force_simple_split: true, strip_chars_from_headers: /[\-"]/ }.freeze
 
-  attr_accessor :ssp_deal_id,
-                :date,
-                :ad_unit,
-                :ad_requests,
-                :impressions,
-                :price,
-                :revenue_loc,
-                :win_rate,
-                :curr_cd,
-                :ssp_advertiser_name,
-                :company,
-                :user
+  attr_reader :user, :company, :file, :pmp_ids, :pmp_item_ids, :import_log, :advertisers, :pmp_change
 
-  validates :date, :ad_unit, :company, presence: true
-  validates :ad_requests, :impressions, presence: true, numericality: true
-  validates :win_rate, numericality: true, allow_nil: true
-  validate :validate_pmp_item_presence
-  validate :validate_deal_id
-  validate :validate_date_format
-  validate :validate_ecpm
-  validate :validate_revenue_loc
-  
-  def initialize(attributes = {})
-    attributes.each do |name, value|
-      send("#{name}=", value)
-    end
+  def self.import(*args)
+    new(*args).import
   end
 
-  def error_messages
-    errors.messages.keys.map do |attr| 
-      if attr == :base
-        errors.full_messages_for(attr)
-      else
-        errors.full_messages_for(attr).first
-      end
-    end.flatten
-  end
+  def initialize(file, user_id, file_path)
+    @user           = User.find(user_id)
+    @company        = user&.company
 
-  def save
-    pmp_item_daily_actual.date = parsed_date
-    pmp_item_daily_actual.ad_unit = ad_unit
-    pmp_item_daily_actual.pmp_item = pmp_item
-    pmp_item_daily_actual.ad_requests = ad_requests
-    pmp_item_daily_actual.impressions = impressions
-    pmp_item_daily_actual.win_rate = win_rate
-    pmp_item_daily_actual.price = price
-    pmp_item_daily_actual.revenue_loc = revenue_loc
-    pmp_item_daily_actual.imported = true
-    pmp_item_daily_actual.ssp_advertiser = ssp_advertiser_name
-    pmp_item_daily_actual.advertiser = advertiser
-    pmp_item_daily_actual.save!
-  end
+    return unless company
 
-  def pmp_item
-    @_pmp_item ||= PmpItem.find_by(ssp_deal_id: ssp_deal_id)
-  end
-
-  def pmp_item_daily_actual
-    @_pmp_item_daily_actual ||= if pmp_item.present?
-      ::PmpItemDailyActual.find_or_initialize_by(pmp_item_id: pmp_item.id, date: parsed_date)
-    else
-      ::PmpItemDailyActual.new
-    end
-  end
-
-  def product
-    return nil if self.ad_unit.blank?
-    ad_unit = AdUnit.where('lower(name) = ?', self.ad_unit.downcase).first
-    if ad_unit
-      ad_unit.product
-    else
-      Product.where('lower(name) = ?', self.ad_unit.downcase).first
-    end
-  end
-
-  def self.import(file, current_user_id, file_path)
-    user = User.find(current_user_id)
-    company = user&.company
-    return unless company.present?
-
-    pmp_item_ids = []
-    pmp_ids = []
-    pmp_change = {time_period_ids: [], product_ids: [], user_ids: []}
-    import_log = CsvImportLog.new(company_id: company.id, object_name: 'pmp_item_daily_actual', source: 'ui')
+    @file           = file
+    @pmp_ids        = []
+    @pmp_item_ids   = []
+    @import_log     = CsvImportLog.new(company_id: company.id, object_name: 'pmp_item_daily_actual', source: 'ui')
     import_log.set_file_source(file_path)
 
-    SmarterCSV.process(file, { force_simple_split: true, strip_chars_from_headers: /[\-"]/ }).each do |row|
+    ssp_advertisers = company.ssp_advertisers.pluck(:name, :client_id).to_h
+    @advertisers    = ssp_advertisers.merge(company.clients.pluck(:name, :id).to_h)
+    @pmp_change     = { time_period_ids: [], product_ids: [], user_ids: [] }
+  end
+
+  def import
+    SmarterCSV.process(file, CSV_OPTIONS).each do |row|
       import_log.count_processed
 
-      csv_pmp_item_daily_actual = self.build(row, user)
-      if csv_pmp_item_daily_actual.valid?
-        begin
-          csv_pmp_item_daily_actual.save
-          import_log.count_imported
+      daily_actual = find_or_build_daily_actual(prepare_params(row))
 
-          pmp_item_ids << csv_pmp_item_daily_actual.pmp_item.id
-          pmp_ids << csv_pmp_item_daily_actual.pmp_item.pmp.id
+      if validate_daily_actual(daily_actual, row)
+        daily_actual.save
+        import_log.count_imported
 
-          pmp = csv_pmp_item_daily_actual.pmp_item.pmp
-          pmp_change[:time_period_ids] += company.time_periods.for_time_period(pmp.start_date, pmp.end_date).collect{|item| item.id}
-          pmp_change[:user_ids] += pmp.pmp_members.collect{|item| item.user_id}
-          pmp_change[:product_ids] += [csv_pmp_item_daily_actual.pmp_item&.product_id]
-        rescue Exception => e
-          import_log.count_failed
-          import_log.log_error ['Internal Server Error', row.to_h.compact.to_s, e.class]
-          next
-        end
+        pmp_item_ids << daily_actual.pmp_item.id
+        pmp_ids << daily_actual.pmp_item.pmp.id
+
+        update_pmp_change(daily_actual.pmp_item)
       else
         import_log.count_failed
-        import_log.log_error csv_pmp_item_daily_actual.error_messages
-        next
+        import_log.log_error daily_actual.errors.full_messages
       end
     end
 
     import_log.save
 
-    PmpItemMonthlyActual.generate(pmp_item_ids.uniq)
-    PmpItem.calculate(pmp_item_ids.uniq)
-    Pmp.calculate_dates(pmp_ids.uniq)
-
-    pmp_change[:time_period_ids] = pmp_change[:time_period_ids].uniq
-    pmp_change[:user_ids] = pmp_change[:user_ids].uniq
-    pmp_change[:product_ids] = pmp_change[:product_ids].uniq
-
-    ForecastPmpRevenueCalculatorWorker.perform_async(pmp_change)
+    recalculate_dates_and_budgets
+    schedule_forecast_recalculation
   end
 
   private
 
-  def advertiser
-    advertiser = client || ssp_advertiser
-    if advertiser.present? && ssp_advertiser_name.present?
-      SspAdvertiser.create_or_update(ssp_advertiser_name, advertiser.id, pmp_item&.ssp&.id, user) 
+  def prepare_params(row)
+    pmp_item = PmpItem.find_by(ssp_deal_id: row[:dealid])
+    date = parse_date(row[:date]&.strip)
+
+    {
+      date:           date,
+      ad_unit:        row[:ad_unit],
+      pmp_item_id:    pmp_item&.id,
+      ad_requests:    row[:ad_requests],
+      impressions:    row[:impressions],
+      win_rate:       row[:win_rate],
+      price:          row[:ecpm],
+      revenue_loc:    row[:revenue],
+      imported:       true,
+      ssp_advertiser: row[:ssp_advertiser],
+      advertiser_id:  advertiser_id(row[:ssp_advertiser], pmp_item),
+      md5_signature:  md5_signature(date, row[:dealid], row[:ad_unit], row[:ssp_advertiser])
+    }
+  end
+
+  def find_or_build_daily_actual(params)
+    daily_actual = ::PmpItemDailyActual.find_or_initialize_by(md5_signature: params[:md5_signature])
+    daily_actual.assign_attributes(params.slice(*ALLOWED_ATTRIBUTES))
+    daily_actual
+  end
+
+  def parse_date(string)
+    return if string.nil?
+
+    date_format = string[/\d{4}/].present? ? '%m/%d/%Y' : '%m/%d/%y'
+    Date.strptime(string, date_format)
+  end
+
+  def advertiser_id(ssp_advertiser_name, pmp_item)
+    return if ssp_advertiser_name.nil?
+
+    advertiser_id = advertisers[ssp_advertiser_name]
+
+    if advertiser_id.present?
+      SspAdvertiser.create_or_update(ssp_advertiser_name, advertiser_id, pmp_item&.ssp&.id, user)
     end
-    advertiser
+
+    advertiser_id
   end
 
-  def client
-    @_client ||= company.clients.find_by(name: ssp_advertiser_name)
+  def md5_signature(date, ssp_deal_id, ad_unit, ssp_advertiser_name)
+    Digest::MD5.hexdigest("#{date}#{ssp_deal_id}#{ad_unit}#{ssp_advertiser_name}")
   end
 
-  def ssp_advertiser
-    @_ssp_advertiser ||= company.ssp_advertisers.find_by(name: ssp_advertiser_name)&.client
+  def update_pmp_change(pmp_item)
+    pmp = pmp_item.pmp
+
+    pmp_change[:time_period_ids] += company.time_periods.for_time_period(pmp.start_date, pmp.end_date).collect{|item| item.id}
+    pmp_change[:user_ids] += pmp.pmp_members.collect{|item| item.user_id}
+    pmp_change[:product_ids] << pmp_item&.product_id
   end
 
-  def validate_ecpm
-    validate_numeric('eCPM', price)
+  def recalculate_dates_and_budgets
+    PmpItemMonthlyActual.generate(pmp_item_ids.uniq)
+    PmpItem.calculate(pmp_item_ids.uniq)
+    Pmp.calculate_dates(pmp_ids.uniq)
   end
 
-  def validate_revenue_loc
-    validate_numeric('Revenue', revenue_loc)
+  def schedule_forecast_recalculation
+    pmp_change[:time_period_ids].uniq!
+    pmp_change[:user_ids].uniq!
+    pmp_change[:product_ids].uniq!
+
+    ForecastPmpRevenueCalculatorWorker.perform_async(pmp_change)
   end
 
-  def validate_numeric(name, val)
-    if val.blank?
-      errors.add(:base, "#{name} can't be blank") 
-    elsif val.is_a? String
-      Float(val) rescue errors.add(:base, "#{name} is not a number") 
-    elsif !val.is_a? Numeric
-      errors.add(:base, "#{name} is not a number")
-    end
-  end
+  def validate_daily_actual(daily_actual, row)
+    daily_actual.valid?
 
-  def validate_pmp_item_presence
-    errors.add(:pmp_item, "with Deal-Id #{ssp_deal_id} could not be found") if pmp_item.nil? && !ssp_deal_id.blank?
-  end
+    daily_actual.errors.tap do |errors|
+      errors.add(:base, "Deal-ID can't be blank") if row[:dealid].blank?
+      errors.add(:pmp_item, "with Deal-Id #{row[:dealid]} could not be found") unless daily_actual.pmp_item
+      errors.add(:date, "- #{row[:date]} must be in valid date format") unless daily_actual.date
 
-  def validate_date_format
-    errors.add(:date, "- #{date} must be in valid date format") unless date_valid?
-  end
-
-  def validate_deal_id
-    errors.add(:base, "Deal-ID can't be blank") if ssp_deal_id.blank?
-  end
-
-  def parsed_date
-    @_parsed_date ||= if date.present? && date_valid?
-      d = Date.strptime(date, "%m/%d/%Y") 
-      if d && d.year < 100
-        d = Date.strptime(date, "%m/%d/%y")
+      if errors[:price].any?
+        errors.delete(:price).each { |error| errors.add('eCPM', error) }
       end
-      d
-    end
-  end
 
-  def date_valid?
-    begin
-      Date.strptime(date, "%m/%d/%Y") if date.present?
-    rescue ArgumentError
-      return false
+      if errors[:revenue_loc].any?
+        errors.delete(:revenue_loc).each { |error| errors.add(:Revenue, error) }
+      end
     end
-    return true
-  end
 
-  def self.build(row, user)
-    Csv::PmpItemDailyActual.new(
-      ssp_deal_id: row[:dealid],
-      date: row[:date].try(:strip),
-      ad_unit: row[:ad_unit],
-      ad_requests: row[:ad_requests],
-      impressions: row[:impressions],
-      win_rate: row[:win_rate],
-      price: row[:ecpm],
-      revenue_loc: row[:revenue],
-      curr_cd: row[:currency],
-      ssp_advertiser_name: row[:ssp_advertiser],
-      company: user&.company,
-      user: user
-    )
+    daily_actual.errors.empty?
   end
 end
