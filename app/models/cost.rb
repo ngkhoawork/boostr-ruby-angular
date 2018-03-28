a358816ced752d0dfaba8d162480714795c60169
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-03-29 05:43:35 +0900

    fix feedback

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -17,7 +17,7 @@ class Cost < ActiveRecord::Base
   scope :for_product_ids, -> (product_ids) { where("product_id in (?)", product_ids) }
 
   after_create do
-    Cost::AmountsGenerateService.new(self).perform if cost_monthly_amounts.count == 0
+    Cost::AmountsGenerateService.new(self).perform if cost_monthly_amounts.empty?
   end
 
   after_destroy do
