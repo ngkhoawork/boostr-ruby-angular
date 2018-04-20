f6aea78dbacb07c76bedc20c2de71798fdca145c
Dmytro Shyrshov <shirshov.dmitriy@gmail.com>
2018-04-21 00:26:43 +0300

    Added tests

---
+++ spec/models/display_line_item_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe DisplayLineItem, type: :model do
+  it 'closes deal display product when line item comes from datafeed' do
+    deal_product = create :deal_product, deal: deal, product: display_product
+
+    deal.update(stage: closed_won_stage, updated_by: 0)
+
+    expect(deal.reload.io).to be_present
+
+    expect(deal_product.reload.open).to be true
+
+    create :display_line_item, io: deal.io, dont_update_parent_budget: true
+
+    expect(deal_product.reload.open).to be false
+  end
+
+  def company
+    @company ||= create :company
+  end
+
+  def deal
+    @deal ||= create :deal, company: company
+  end
+
+  def closed_won_stage
+    @_won_stage ||= create(:won_stage, company: company, open: false)
+  end
+
+  def display_product
+    @display_product ||= create(:product, revenue_type: 'Display', company: company)
+  end
+end
