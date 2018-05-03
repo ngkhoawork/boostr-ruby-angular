773cfc2290e3f1eea05c3545ef3be47029529aaf
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-05-03 19:07:40 +0300

    BOOS-1637 fix escaping of amount

--- app/models/csv/contract.rb
+++ app/models/csv/contract.rb
@@ -53,7 +53,7 @@ class Csv::Contract
       c.end_date = formatted_date(end_date)
       c.auto_notifications = true?(auto_notifications)
       c.currency = currency
-      c.amount = amount.to_d
+      c.amount = amount&.to_d
       c.description = description
       c.days_notice_required = days_notice_required.to_i
       c.deal = deal
