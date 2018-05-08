0fed765b9ce6fd86aebdfb16523e42f6d1aac427
Khoa Ngyuen <ngkhoawork@gmail.com>
2018-05-08 16:04:13 +0300

    BOOS-1588 reset schema and add starting page to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < Users::BaseSerializer
   attributes :roles_mask, :cycle_time, :is_active, :is_admin, :employee_id,
     :leads_enabled, :user_type, :revenue_requests_access, :title, :contracts_enabled,
-    :is_legal, :default_currency
+    :is_legal, :default_currency, :starting_page
 
   has_one :team, serializer: TeamSerializer
   has_many :teams, serializer: TeamSerializer
