c98a44b814ba02deb07c43f0d4223eb479209d2d
Yaroslav Hashparych <slavikggg@gmail.com>
2018-05-10 18:12:54 +0300

    BOOS-1639 Added validation of uniqueness for custom field names position
    - Moved common logic for Custom field names into concern
    - Added validation of presence of field type for each custom field name
    - Refactored some code
    - Added tests

--- app/controllers/api/account_cf_names_controller.rb
+++ app/controllers/api/account_cf_names_controller.rb
@@ -31,22 +31,7 @@ class Api::AccountCfNamesController < ApplicationController
   end
 
   def create
-    index_arr = account_cf_names.by_type(account_cf_name_params[:field_type]).collect{ |item| item.field_index }
-    field_index = 0
-
-    field_limit = AccountCfName.get_field_limit(account_cf_name_params[:field_type])
-    for i in 1..field_limit
-      if index_arr.index(i).nil?
-        field_index = i
-        break
-      end
-    end
-    if field_index == 0
-      render json: { errors: {"This field type" => [" reached limit."]} }, status: :unprocessable_entity
-      return
-    end
     account_cf_name = account_cf_names.new(account_cf_name_params)
-    account_cf_name.field_index = field_index
 
     if account_cf_name.save
       render json: account_cf_name, status: :created
@@ -73,10 +58,8 @@ class Api::AccountCfNamesController < ApplicationController
 
   def account_cf_name_params
     params.require(:account_cf_name).permit(
-            :field_type, :field_label, :is_required, :position, :show_on_modal, :disabled,
-            {
-                    account_cf_options_attributes: [:id, :value]
-            }
+      :field_type, :field_label, :is_required, :position, :show_on_modal, :disabled,
+      { account_cf_options_attributes: [:id, :value] }
     )
   end
 
