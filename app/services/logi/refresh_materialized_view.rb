f77069a0127ab1e5b045ec69052f908b7979fcc7
Vitaleks-project <vitaliy.jorzh100592@gmail.com>
2018-06-06 11:30:40 +0300

    [BOOSTR-1733] update executed query for refresh view;

--- app/services/logi/refresh_materialized_view.rb
+++ app/services/logi/refresh_materialized_view.rb
@@ -1,7 +1,7 @@
 class Logi::RefreshMaterializedView
   def self.perform
-    db = ActiveRecord::Base.establish_connection
-    connection = db.connection
-    connection.execute('REFRESH MATERIALIZED VIEW vw_prod_mon_sum_sa_2017;')
+    ActiveRecord::Base.connection.execute <<-SQL
+      REFRESH MATERIALIZED VIEW vw_prod_mon_sum_sa_2017;
+    SQL
   end
 end
